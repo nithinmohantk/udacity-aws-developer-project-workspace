94285ea296da31511db289ffc54da8dbcd12f331
Nithin Mohan T K <nithinmohantk@hotmail.com>
2020-04-02 21:16:08 +0100

    type: Udagram Image Filter Service final code
    Body:
    1. **Used 'bestzip' npm component**: for creating archive.zip
    In Windows build command to create archive.zip will not work, as there is no 'zip' exec by default in windows.
    2. src\tmp\dummy.ts - temporary workaround  to make the "tmp" folder copied to www folder, as npm build was failing due to lack of permission to execute 'mkdir www/tmp', as my working folder was in Windows Desktop, and lacks permission.
    3. **package.json** - modified npm commands to fit the needs.
    4. **npm audit fix** && **package-lock.json** - to fix issues.
    5. **server.ts**  - implemented the following code and added console logging.
    
    footer:
    Completes Udagram Image Filter Service assignment

---
+++ project/image-filter-udagram-app/CHANGES.md
@@ -0,0 +1,5 @@
+In Windows build command to create archive.zip will not work, as there is no 'zip' exec by default in windows.
+
+Used 'bestzip' npm component instead. 
+
+
--- project/image-filter-udagram-app/src/util/util.ts
+++ project/image-filter-udagram-app/src/util/util.ts
@@ -9,16 +9,25 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+    return new Promise( async (resolve, reject) => {
+
+            const photo = await Jimp.read(inputURL);
+            const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+
+            if(photo)
+            {
+                await photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname+outpath, (img)=>{
+                    resolve(__dirname+outpath);
+                });
+            }
+            else 
+            {
+                reject(new Error('woops'));
+            }
     });
 }
 
