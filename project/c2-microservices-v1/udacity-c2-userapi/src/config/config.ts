bb79aa08304dda52a759b969339e495560d7d196
Nithin Mohan T K <nithinmohantk@hotmail.com>
2020-04-03 11:25:00 +0100

    Separated the working udacity-c2-restapi to user,feed individual services

---
+++ project/c2-microservices-v1/udacity-c2-userapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": "eu-west-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-nithin-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt" : {
+    "secret": process.env.JWT_SECRET
+  }
+}
